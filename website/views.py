698b889a0812bf290f6873d6b62a8424e8861600
Clive <benclive@acbc32ae5c0d.ant.amazon.com>
2016-04-05 21:36:21 +0100

    Adding display of people and performances

--- website/views.py
+++ website/views.py
@@ -2,12 +2,19 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template.loader import render_to_string
 
+from website.models import Ringer,Performance
+
 from . import tasks
 
 # Create your views here.
 
 def index(request):
-    rendered = render_to_string('website/index.html')
+    ringers = Ringer.objects.all();
+    sorted(ringers, key=lambda ringer: ringer.name)
+
+    performances = Performance.objects.all();
+
+    rendered = render_to_string('website/index.html', {'ringers': ringers, 'performances': performances})
 
     return HttpResponse(rendered);
 
